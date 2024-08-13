cbcef1f58f2b5526038402a3962e72c5d4aa79d8
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-08-13 16:22:22 +0300

    feat: add classname prop

--- src/components/TickerItem.tsx
+++ src/components/TickerItem.tsx
@@ -1,10 +1,12 @@
+import clsx from 'clsx'
+import { CommonProps } from '../types/common'
 import { TickerLogo } from './TickerImage'
 
 type TickerItem = {
   displayName: string | null
   ticker: string
   price: string
-}
+} & CommonProps
 
 export const TickerItem = (props: TickerItem) => {
   const floatPrice = parseFloat(props.price)
@@ -13,7 +15,11 @@ export const TickerItem = (props: TickerItem) => {
   const mainSymbol = props.ticker.replace('USDT', '')
 
   return (
-    <div className='border shadow-sm flex items-center gap-2 overflow-hidden rounded-md min-w-[170px] p-2 shrink-0'>
+    <div
+      className={clsx(
+        'border shadow-sm flex items-center gap-2 overflow-hidden rounded-md min-w-[170px] p-2 shrink-0 bg-white',
+        props.className
+      )}>
       <TickerLogo className='w-8 h-8 border rounded-full p-0.5' ticker={mainSymbol} />
       <div className='flex flex-col md:text-xl'>
         {props.displayName ? (
