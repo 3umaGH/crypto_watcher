255842319698d0f121a7bb8b88765bfdc2459a48
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-08-13 14:25:42 +0300

    feat: implement paper component

---
+++ src/components/layout/Paper.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx'
+import { ReactNode } from 'react'
+import { CommonProps } from '../../types/common'
+
+type Paper = { children: ReactNode } & CommonProps
+
+export const Paper = (props: Paper) => {
+  return (
+    <div
+      className={clsx(
+        'border border-gray-200/80 bg-[#fafafa] rounded-md overflow-hidden drop-shadow-md p-2',
+        props.className
+      )}>
+      {props.children}
+    </div>
+  )
+}
