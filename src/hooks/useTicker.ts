b81e7421eae96d4a501f09c9b3cddb11a09c0a54
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-08-13 15:56:14 +0300

    refactor: for better readability

--- src/hooks/useTicker.ts
+++ src/hooks/useTicker.ts
@@ -1,5 +1,9 @@
 import { useContext } from 'react'
 import { TickerContext } from '../context/tickerContext'
+import { Ticker } from '../types/common'
+
+const NAME_FILTER = (ticker: Ticker, query: string) => ticker.displayName?.toLowerCase().startsWith(query.toLowerCase())
+const SYMBOL_FILTER = (ticker: Ticker, query: string) => ticker.symbol.toLowerCase().startsWith(query.toLowerCase())
 
 export const useTicker = () => {
   const { tickers } = useContext(TickerContext)
@@ -9,12 +13,18 @@ export const useTicker = () => {
   }
 
   const findByDisplayName = (query: string) => {
-    return tickers.filter(ticker => ticker.displayName?.toLowerCase().startsWith(query.toLowerCase()))
+    return tickers.filter(ticker => NAME_FILTER(ticker, query))
   }
 
   const findBySymbol = (query: string) => {
-    return tickers.filter(ticker => ticker.symbol.toLowerCase().includes(query.toLowerCase()))
+    return tickers.filter(ticker => SYMBOL_FILTER(ticker, query))
   }
 
-  return { getTickers, findByDisplayName, findBySymbol }
+  const find = (query: string) => {
+    return tickers.filter(ticker => {
+      return NAME_FILTER(ticker, query) || SYMBOL_FILTER(ticker, query)
+    })
+  }
+
+  return { getTickers, findByDisplayName, findBySymbol, find }
 }
